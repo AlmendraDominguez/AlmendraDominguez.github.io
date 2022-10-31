887c50860208c1ed79bd7bed468eec2cb04ad94c
almendra dominguez <almendradominguez97@gmail.com>
2022-10-31 20:24:51 -0300

    tp1

--- tickets.js
+++ tickets.js
@@ -1,71 +1,76 @@
 function tickets() {
     const divContent = document.getElementById('comprarTicket');
-    const content = ` <div class="d-flex justify-content-center">
-    <div class="card-group card text-center col-4">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Estudiante</h5>
-            <p class="card-text">Tienen un descuento</p>
-            <p class="card-text"><b>80%</b></p>
+    const content = `<div class="d-flex justify-content-center">
+    <div class="card text-center col border-primary border-2 me-1">
+        <div class="card-body px-0">
+            <h4 class="card-title">Estudiante</h4>
+            <p class="card-text fs-5">Tienen un descuento</p>
+            <h5 class="card-title"><b>80%</b></h5>
             <p class="card-text"><small class="text-muted">*presentar documentacion</small></p>
-        </div>
         </div>
     </div>
 
-    <div class="card-group card text-center col-4">
-        <div class="card">
+    <div class="card text-center col border-info border-2 mx-1">
         <div class="card-body">
-            <h5 class="card-title">Trainee</h5>
-            <p class="card-text">Tienen un descuento</p>
-            <p class="card-text"><b>50%</b></p>
+            <h4 class="card-title">Trainee</h4>
+            <p class="card-text fs-5">Tienen un descuento</p>
+            <h5 class="card-title"><b>50%</b></h5>
             <p class="card-text"><small class="text-muted">*presentar documentacion</small></p>
-        </div>
         </div>
     </div>
 
-    <div class="card-group card text-center col-4">
-        <div class="card">
+    <div class="card text-center col border-warning border-2 ms-1">
         <div class="card-body">
-            <h5 class="card-title">Junior</h5>
-            <p class="card-text">Tienen un descuento</p>
-            <p class="card-text"><b>15%</b></p>
+            <h4 class="card-title">Junior</h4>
+            <p class="card-text fs-5">Tienen un descuento</p>
+            <h5 class="card-title"><b>15%</b></h5>
             <p class="card-text"><small class="text-muted">*presentar documentacion</small></p>
-        </div>
         </div>
     </div>
 </div>
 
-</div>
 </div>
 </section>
-<section class="container-fluid">
+<section class="container">
+<div class="row d-flex justify-content-center">
+<form>
+<div class="row text-center">
+    <p>VENTA</p>
+    <h1>VALOR DE TICKET $200</h1>
+</div>
+
 <div class="row">
-<div>
-<p>VENTA</p>
-<p><b>VALOR DE TICKET $200</b></p>
+    <input type="text" style="width: auto;" class="form-control col me-4" placeholder="Nombre" aria-label="Nombre">
 
-<input type="text" class="form-control col-6" placeholder="Nombre" aria-label="Nombre">
-<input type="text" class="form-control col-6" placeholder="Apellido" aria-label="Apellido">
-<input type="text" class="form-control" placeholder="Correo" aria-label="Correo">
-<label for="basic-url" class="form-label">Cantidad</label>
-<div class="input-group mb-3">
-    <input type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad">
+    <input type="text" style="width: auto;" class="form-control col" placeholder="Apellido" aria-label="Apellido">
 </div>
-<label for="basic-url" class="form-label">Categoria</label>
-<select class="form-select" aria-label="Default select example">
-    <option selected value="1">Estudiante</option>
-    <option value="2">Trainee</option>
-    <option value="3">Junior</option>
-</select>
-<input type="text" class="" style="background-color: rgba(204,229,255,255)" placeholder="Total a pagar: $">
+<div class="row mt-4">
+    <input type="text" class="form-control" placeholder="Correo" aria-label="Correo">
+</div>
+<div class="row">
+    <div class="cantidad col mt-3 px-0">
+        <label for="basic-url" class="form-label col">Cantidad</label>
+        <input type="text" class="form-control col" placeholder="Cantidad" aria-label="Cantidad">
+    </div>
+    <div class="categoria col mt-3 ps-4 pe-0">
+        <label for="basic-url" class="form-label col">Categoria</label>
+        <select class="form-select col pe-0" aria-label="Default select example">
+            <option selected value="1">Estudiante</option>
+            <option value="2">Trainee</option>
+            <option value="3">Junior</option>
+        </select>
+    </div>
+</div>
+<div class="row mt-4">
+    <p class="border rounded p-2" style="background-color: rgba(204,229,255,255); height: 45px;">Total a pagar: $</p>
+</div>
 
-<div>
-    <button type="button" class="btn miBoton text-white col-12" style="background-color: rgba(150,201,62,255)">Borrar</button>
+<div class="row my-4">
+    <button type="button" class="btn miBoton text-white col me-3" style="background-color: rgba(150,201,62,255)">Borrar</button>
+    
+    <button type="button" class="btn miBoton text-white col ms-2" style="background-color: rgba(150,201,62,255)">Resumen</button>
 </div>
-<br>
-<div>
-    <button type="button" class="btn miBoton text-white col-12" style="background-color: rgba(150,201,62,255)">Resumen</button>
-</div>`;
+</form>`;
 
   divContent.innerHTML = content;
 }
